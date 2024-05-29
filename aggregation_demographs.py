70618beae2c567aca25983a2f18cd33563dc69a4
JeongEul Kwon <wjddmf0321@naver.com>
2024-05-29 14:09:54 +0900

    Code

---
+++ aggregation_demographs.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import gc
+import os
+from tqdm import tqdm
+import warnings
+pd.set_option('mode.chained_assignment',  None) 
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+local = '/Users/DAHS/Desktop/hirid-a-high-time-resolution-icu-dataset-1.1.1/'
+
+def P_Aggregation_Demograph(part_list):
+    print("[ EXCUTING AGGREGATION & GET DEMOGRAPHS ]")
+    result = pd.DataFrame()
+    patient = pd.read_csv('/Users/DAHS/Desktop/hirid-a-high-time-resolution-icu-dataset-1.1.1/reference_data/general_table.csv', usecols=['patientid', 'sex', 'age'])
+    for parts in part_list:
+        # print(f'Part {parts} processing......')
+
+        part = pd.read_csv(f'/Users/DAHS/Desktop/hirid-a-high-time-resolution-icu-dataset-1.1.1/raw_stage/tabular_records/csv_labeling/part-{parts}.csv')
+        demo = pd.merge(part, patient, how = 'left', on='patientid')
+        result = pd.concat([result, demo], axis = 0)
+    
+    result['sex'] = result['sex'].replace({'M' : 1, 'F':0})
+    result.reset_index(drop=True, inplace=True)
+    
+    result = result.rename(columns = {'sex':'Sex', 'age':'Age', 'Platelet_count':'Platelet_Count', 'Platelet_count_up':'Platelet_Count_up', 'Urine_output':'Urine_Output'})
+    print('=======')
+    print('Num of obs: ', len(result))
+    print('num of patient: ', result.patientid.nunique())
+    print('MAN: ', len(result[result['Sex'] == 1]))
+    print('WOMAN: ', len(result[result['Sex'] == 0]))
+    print('=======')
+    result.to_csv(local + 'hirid.csv.gz',index=False)
+    
+    print("[ FINISH AGGREGATION & GET DEMOGRAPHS ]")
+    
